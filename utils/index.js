ec821d9be0d10c3628563d6cf091ca5e93fafca6
brilliantchanjr@gmail.com <brilliantchanjr@gmail.com>
2021-10-06 09:48:57 +0800

    新增公共方法

--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,26 @@
 const sleep = (ms = 500) => new Promise((resolve) => setTimeout(resolve, ms));
 
+/**
+ * 匹配正则表达输出所有位置
+ * @param {*} reg 正则表达
+ * @param {string} str 目标字符串
+ * @returns {array}
+ */
+const matchIndex = (reg, str) => {
+    const newReg = new RegExp(reg, 'g');
+    const array = [];
+
+    while ((result = newReg.exec(str)) !== null) {
+        const fIndex = newReg.firstIndex;
+        const lIndex = newReg.lastIndex - result[0].length;
+
+        array.push(lIndex);
+    }
+
+    return array;
+}
+
 export {
-    sleep
+    sleep,
+    matchIndex
 };
